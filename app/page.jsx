e98f8756f764f1b7c41bd768be9a273e61df99c9
ahmetanil1 <amtyrgnc@gmail.com>
2024-10-20 17:07:43 +0300

    First Commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomeContainer from '@/containers/home'
+function Page() {
+    return (
+        <div>
+            <HomeContainer />
+        </div>
+    )
+}
+
+export default Page
---
+++ components/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./styles.module.css";
+
+function footer() {
+    return (
+        <footer className={styles.footer}>
+            @ Made by Ahmet Yorgancı
+        </footer>
+    )
+}
+
+export default footer
